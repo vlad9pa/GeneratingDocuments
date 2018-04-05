90512f34179dc6bc1970934102d8b146f6de280c
Vlad Miliutin <vlad.milytin@gmail.com>
2018-04-05 20:11:40 +0300

    Simple Generating Pdf page

---
+++ src/main/java/com/example/documents/DocumentsApplication.java
@@ -0,0 +1,12 @@
+package com.example.documents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentsApplication.class, args);
+	}
+}
